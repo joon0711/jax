6b40bd51b8dfde85bef01eb200d0e24b446fd608
joon0711 <joon@cohere.com>
2022-11-17 15:23:01 +0000

    Add print

--- jax/experimental/compilation_cache/compilation_cache.py
+++ jax/experimental/compilation_cache/compilation_cache.py
@@ -126,6 +126,7 @@ def _hash_computation(hash_obj, xla_computation):
   else:
     serialized_hlo = xla_computation.as_serialized_hlo_module_proto()
   scrubbed_hlo = re.sub(b" at 0x[a-f0-9]+>", b" at 0x...>", serialized_hlo)
+  print(scrubbed_hlo)
   scrubbed_hlo = re.sub(b"/.*\.py", b"", scrubbed_hlo)
   hash_obj.update(scrubbed_hlo)
 
